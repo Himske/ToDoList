d95477300d8bb7af9dbca7249c7b0e953547bd73
Fredrik Nilsson <himske@gmail.com>
2026-09-24 09:53:07 +0200

    Cleaned up some whitespace

--- ToDoList/ToDo.cs
+++ ToDoList/ToDo.cs
@@ -13,12 +13,10 @@ namespace ToDoList {
         }
 
         public int Id { get; set; }
-
         public string Title { get; set; } = string.Empty;
         public DateTime DueDate { get; set; }
         public Status Status { get; set; }
         public string Project { get; set; } = string.Empty;
-
         public DateTime UpdateDate { get; set; } = DateTime.Now;
     }
 }
